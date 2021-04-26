d2b649dba3e51f809fab4433ff3d8a8c43146153
NithishWhitehatjr <81054136+NithishWhitehatjr@users.noreply.github.com>
2021-04-25 17:59:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function getParagraph1()
+{
+var inputs = []
+for (var i = 1 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById ("para1_input_box" + i).value);
+}
+inputs.join (". ")
+document.getElementById("showparagraph2").html = inputs.join (". ")
+function getParagraph1()
+{
+var inputs = []
+for (var i = 2 ; i <=6 ; i++)
+{
+    inputs.push(document.getElementById ("para1_input_box" + i).value);
+}
+inputs.join (". ")
+document.getElementById("showparagraph2").html = inputs.join (". ")
